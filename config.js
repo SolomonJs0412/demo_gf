c1d51a339d4f6be37e1ef5c4675368706d29d801
solomonjs0412 <vothuykg123@gmail.com>
2022-10-03 22:46:01 +0700

    done

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Umoaaa';
+const giftUrl = '';
+const eventName = 'Tặng cho Len Laverna';
+const titleCard = 'Bé Len đáng iuuu';
+const contentCard = 'Hi vọng bé Len sớm khỏi bệnh và có một cuộc đời thật vui vẻ, cảm ơn em vì đã đến với anh, đây là 1 đoạn code anh thấy từ trên website, coi như là demo cho món quà anh sẽ dành tặng em vào ngày mai 4/10/2022';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '9ecdf55b6506a258fb17.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
